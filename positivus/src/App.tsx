77172db0aa04b63e513f07a696d634e53bb2377a
Luis Felipe <83621659+Chiqueto@users.noreply.github.com>
2024-07-30 10:06:45 +0000

    criação do projeto REACT

---
+++ positivus/src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from './assets/Icon.svg';
+import './index.css';
+export function App() {
+
+  return (
+    <div className="m-24">
+      teste
+      <header>
+        <div className="m-24"><img src={Logo}/></div>
+        <div></div>
+      </header>
+    </div>
+  )
+}
